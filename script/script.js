f7c69b90995eb9f3bea57ce761484bd6915d97b4
Claiverty <claiverty14@gmail.com>
2025-08-13 10:35:00 -0300

    Conecatando o script ao supabase para salvar as tabelas

--- script/script.js
+++ script/script.js
@@ -15,13 +15,11 @@ const _supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 function updateUserInterface(user) {
   if(user) {
     //usuario ta logado
-    console.log("Usuário está logado:", user)
     statusMessage.textContent = `Logado como: ${user.user_metadata.full_name}. Vote agora!`
     loginButton.classList.add("hidden") // esconde btn login
     formVotacao.classList.remove("hidden") // mostra o formulario
   } else {
     //usuario deslogado
-    console.log("Nenhum usuário está logado.")
     statusMessage.textContent = "Faça login com o seu Discord para poder votar."
     loginButton.classList.remove("hidden") // mostra btn login
     formVotacao.classList.add("hidden") // esconde o formulario
@@ -30,24 +28,74 @@ function updateUserInterface(user) {
 
 // login com discord
 async function loginComDiscord() {
-  const {error} = await _supabase.auth.signInWithOAuth({
+  await _supabase.auth.signInWithOAuth({
     provider: "discord"
   })
-
-  if(error) {
-    console.error("Erro no login: ", error)
-  }
 }
-
 // chamando a função no botao de login
-loginButton.addEventListener("click", () => {
-  loginComDiscord()
-})
+loginButton.addEventListener("click", loginComDiscord)
+
+// funcao para enviar os votos
+async function handleFormSubit() {
+  event.preventDefault()
+
+  const submitButton = event.target.queryselector("button[type= 'submit']")
+  submitButton.disabled = true
+  submitButton.textContent = "Enviando..."
+
+  //pega id do usuario
+  const {data: {user}} = await _supabase.auth.getUser()
+
+  //verifica se o usuario ja votou
+  const {data: existingVote, error:selectError} = await _supabase
+  .from("votos")
+  .select("id")
+  .eq("user_id", user.id)
+  .single()
+
+  if(selectError && selectError.code != "PGRST116") {
+    alert(`Erro ao verificar seu voto: ${selectError.message}`)
+    submitButton.disabled = false
+    submitButton.textContent = "Enviar Votos"
+    return
+  }
+  
+  if(existingVote) {
+    alert("Você já votou! Obrigaado por participar.")
+    formVotacao.classList.add("hidden")
+    statusMessage.innerHTML = `<h2>Seu voto já foi registrado anteriormente. ✅</h2><p>Obrigado, ${user.user_metadata.full_name}! Os resultados serão divulgados em breve.</p>`
+    return
+  }
+
+  //se nao tiver votado, os dados do form sao enviados
+  const formData = new FormData(formVotacao)
+  const voto = {
+      user_id: user.id,
+      mais_querido: formData.get("mais_querido"),
+      staff_do_ano: formData.get("staff_do_ano"),
+      membro_do_ano: formData.get("membro_do_ano"),
+      membro_mais_ativo: formData.get("membro_mais_ativo"),
+      rei_da_resenha: formData.get("rei_da_resenha"),
+      o_mais_chato: formData.get("o_mais_chato"),
+  }
+  const {error: insertError} = await _supabase.from("votos").insert(voto)
+
+  if(insertError) {
+    alert(`Ocorreu um erro ao registrar seu voto: ${insertError.message}`)
+    submitButton.disabled = false
+    submitButton.textContent = "Enviar Votos"
+  } else {
+    alert("Voto registrado com sucesso! Obrigado por participar!")
+    formVotacao.classList.add("hidden")
+    statusMessage.innerHTML = `<h2>Voto Confirmado! ✅</h2><p>Obrigado, ${user.user_metadata.full_name}! Os resultados serão divulgados em breve no Discord.`
+  }
+  
+}
+formVotacao.addEventListener("submit", handleFormSubit)
+
 
 //verificar o estado da autenticação
 _supabase.auth.onAuthStateChange((event, session) => {
-  //session contem as informacoes do usuario logado
-  // se a sessao existe, session.user tambem existe
   const user = session ? session.user  : null
   updateUserInterface(user)
 })
