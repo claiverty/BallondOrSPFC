10de792a36f7bd71729619fbfea32eb0b52e9af6
Claiverty <claiverty14@gmail.com>
2025-08-13 21:08:08 -0300

    adicionando cronometro

--- script/script.js
+++ script/script.js
@@ -99,4 +99,45 @@ _supabase.auth.onAuthStateChange((event, session) => {
   const user = session ? session.user  : null
   updateUserInterface(user)
 })
+
+//cronometro e data pt-br
+function parseDateBR(dataString) {
+  const partes = dataString.split("")
+  const dataPartes = partes[0].split("/")
+  const tempoPartes = partes[1].split(":")
+
+  return new Date(dataPartes[2], dataPartes[1] - 1, dataPartes[0], tempoPartes[0], tempoPartes[1], tempoPartes[2])
+
+}
+
+if(document.getElementById("cronometro")) {
+  const dataStringBR = "27/09/2025 23:59:59"
+  const dataFinal = parseDateBR(dataStringBR).getTime()
+
+  //atualiza o cronometro a cada 1 segundo
+  const intervalo = setInterval(function(){
+    const agora = new Date().getTime()
+    const distancia = dataFinal - agora
+
+    //calcula o tempo
+    const dias = Math.floor(distancia / (1000 * 60 * 60 * 24))
+    const horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    const minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60))
+    const segundos = Math.floor((distancia % (1000 * 60)) / 1000)
+
+    document.getElementById('dias').innerText = dias.toString().padStart(2, '0')
+    document.getElementById('horas').innerText = horas.toString().padStart(2, '0')
+    document.getElementById('minutos').innerText = minutos.toString().padStart(2, '0')
+    document.getElementById('segundos').innerText = segundos.toString().padStart(2, '0')
+
+    //mensagem exibida quando a contagem acabar
+    if(distancia < 0) {
+      clearInterval(intervalo)
+      const container = document.getElementById("cronometro-container")
+      if(container) {
+        container.innerHTML = "<h2 style='color:#e00000; font-size: 1.5em;'>VOTAÇÃO ENCERRADA!</h2>"
+      }
+    }
+  })
+}
   
